ddd97d8c8b86b469499e87c9e2092772feb00aad
Tatthep <tatthep.c@hoteliers.guru>
2019-03-12 16:59:22 +0700

    try to fetch data

--- src/views/about/sub/Sub1.vue
+++ src/views/about/sub/Sub1.vue
@@ -9,6 +9,25 @@
 export default {
   name: 'sub1',
   props: ['title'],
-  components: {}
+  components: {},
+  async created () {
+    // fetch the data when the view is created and the data is
+    // already being observed
+    console.log('--->>> About Main :: created')
+    await this.fetchData()
+    console.log('--->>> About Main :: created - end')
+  },
+  watch: {
+    // call again the method if the route changes
+    '$route': 'fetchData'
+  },
+  methods: {
+    fetchData () {
+      console.log('--->>> About Main :: fetch')
+      return fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => console.log(json))
+    }
+  }
 }
 </script>
