0183ed73e9cc19a656234b4d01ceafe6a6fecfd6
Tatthep <tatthep.c@hoteliers.guru>
2019-03-12 16:01:08 +0700

    route: try route guard - hook route navigation

--- src/router.js
+++ src/router.js
@@ -18,12 +18,29 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ './views/about/AboutMain.vue'),
-    children: AboutRoute
+    children: AboutRoute,
+    meta: { requiresAuth: true }
+    // beforeEnter: (to, from, next) => {
+    //   console.log(`Before route about..... ${to.name}`)
+    //   next(false)
+    // }
   }
 ]
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: routes
 })
+
+// router.beforeEach((to, from, next) => {
+//   console.log(`Before Router :: to ${to.name} - from ${from.name}`)
+//
+//   if (to.matched.some(record => record.meta.requiresAuth)) {
+//     console.log(`--->>>> ${to.name} require auth`)
+//   }
+//
+//   next()
+// })
+
+export default router
