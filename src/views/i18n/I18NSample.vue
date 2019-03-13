a7910ef17b240ae875ff2566129e7c7d262c0534
Tatthep <tatthep.c@hoteliers.guru>
2019-03-13 14:42:46 +0700

    i18n: use directive v-t

--- src/views/i18n/I18NSample.vue
+++ src/views/i18n/I18NSample.vue
@@ -4,15 +4,28 @@
     <button v-on:click="$i18n.locale = 'en'">EN</button> |
     <button v-on:click="$i18n.locale = 'th'">TH</button>
     <br><br>
+
     <div>
+      <h3>Basic Usage</h3>
       <p>{{ $t('message.hello', { lang: $i18n.locale }) }}</p>
       <p v-html="$t('message.hello', { lang: $i18n.locale })"></p>
     </div>
+
+    <div>
+      <h3>Use Directive instead of $t</h3>
+      <p v-t="'message.hello'"></p>
+      <p v-t="{
+        path: 'message.hello',
+        args: { lang: $i18n.locale }
+      }"></p>
+    </div>
+
     <div>
       <h3>Pluralization</h3>
       <p>1 {{ $tc('car', 1) }}</p>
       <p>2 {{ $tc('car', 2) }}</p>
     </div>
+
   </div>
 </template>
 
